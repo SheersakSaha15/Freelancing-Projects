7b4520d030f43f6396ff0f4240bea1dc07376118
SheersakSaha15 <sheersak.1506@gmail.com>
2026-02-24 04:09:49 +0530

    making the page

---
+++ ScaleArchLandingPage/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <>
+      <link rel="stylesheet" href="/src/index.css"></link>
+      <h1>ScaleArch</h1>
+      <h2>From insight to impact</h2>
+    </>
+
+  )
+}
+
+export default App
